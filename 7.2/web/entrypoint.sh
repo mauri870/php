e4976f8fb3d3075db7bb9c83d2bd330513009d0c
Mauri de Souza Nunes <mauri870@gmail.com>
2018-05-12 18:05:07 -0300

    Add php sockets extension

--- 7.2/web/entrypoint.sh
+++ 7.2/web/entrypoint.sh
@@ -3,7 +3,6 @@
 echo "Aliasing $FRAMEWORK"
 sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
-
 # Starts FPM
 nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
 
