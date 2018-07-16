55a6858499d0cab20586a40616bb869ac3c155b7
Mauri de Souza Nunes <mauri870@gmail.com>
2018-07-16 13:40:15 -0300

    Increase pm.max_children to 30

--- 7.2/cli/entrypoint.sh
+++ 7.2/cli/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -i -e "s/memory_limit = 128M/memory_limit = 512M/g" /etc/php7/php.ini
+sed -i -e "s/pm.max_children = 5/pm.max_children = 30/g" /etc/php7/php-fpm.d/www.conf
+
 sh /scripts/start.sh
 
 sudo crond
